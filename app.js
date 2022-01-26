29412066f943d00f726fbfefe1c84b88c622fd9e
Bastien FUCHS <https://github.com/FuchsBastien/FuchsBastien_5_07092021.git>
2022-01-26 11:10:39 +0100

    correspondance des cartes et verrouillage

--- app.js
+++ app.js
@@ -14,31 +14,90 @@ cartes.forEach(carte => {
 function retourneCarte(){
 
     /*this fait référence à la carte cliquée et childNodes à son enfant*/
-    console.log(this.childNodes);
+    //console.log(this.childNodes);
+
+    //si verrouillage est true
+    if (verouillage) 
+    return
 
     /*on prend l'élèment 2 de l'enfant (div double face et lui rajouter la classe active) 
     l'élèment 1 et 3 compte comme des <>*/
     this.childNodes[1].classList.toggle('active');
+        
+        /*si carteRetournee = false*/
+        if(!carteRetournee){
 
-    /*si carteRetournee = false*/
-    if(!carteRetournee){
+            //on dit carteRetounee est true 
+            carteRetournee = true;
+            console.log(carteRetournee);
+            //on donne à premiereCarte la valeur this (la carte sur laquelle on a cliqué)
+            premiereCarte = this;
+            /*on arrête la fonction (le but est qu'une fois sur deux quand on appelle la fonction est
+            de donner carteRetournee = true */
+            return;
+        }
 
-        //on dit carteRetounee est true 
-        carteRetournee = true;
+        //on dit carteRetounee est false
+        carteRetournee = false;
         console.log(carteRetournee);
-        //on donne à premiereCarte la valeur this (la carte sur laquelle on a cliqué)
-        premiereCarte = this;
-        /*on arrête la fonction (le but est qu'une fois sur deux quand on appelle la fonction est
-        de donner carteRetournee = true */
-        return;
+        //on donne à secondeCarte la valeur this (la carte sur laquelle on a cliqué)
+        secondeCarte = this;
+
+        verouillage = true;
+        console.log(verouillage);
+
+        console.log(premiereCarte, secondeCarte);
+
+        //on applique la fonction
+        correspondance ();
+}
+
+function correspondance(){
+
+    //si les deux carte ont le même attribut
+    if(premiereCarte.getAttribute('data-attr') === secondeCarte.getAttribute('data-attr')) {
+
+        /*on enlève l'évènement par le click de la fonction retournerCarte pour premierCarte et secondCarte
+        (on dit aux cartes de rester retournée)*/
+        premiereCarte.removeEventListener('click', retourneCarte);
+        secondeCarte.removeEventListener('click', retourneCarte);
+
+    } else {
+        //en attendant que le seTimeout s'effectue on ne peut plus cliquer sur d'autres carte
+        verouillage = true;
+         //on laisse les 2 cartes dévoilées non correspondantes pendant 1500 ms pour les mémoriser
+        setTimeout(() => {
+            //on enlève la class active pour retourner les cartes
+
+            premiereCarte.childNodes[1].classList.remove('active');
+            secondeCarte.childNodes[1].classList.remove('active');
+
+            verouillage = false;
+        }, 1500)
     }
 
-    //on dit carteRetounee est false
-    carteRetournee = false;
-    console.log(carteRetournee);
-    //on donne à secondeCarte la valeur this (la carte sur laquelle on a cliqué)
-    secondeCarte = this;
-
-    console.log(premiereCarte, secondeCarte);
-
 }
+
+/*Résumé :
+Pour caque carte cliquée on applique la fonction carteRetournee :
+
+Cette fonction s'arrête si verrouillage (2 cartes cliquées) est true donc n'ajoute plus
+de valeurs à premiereCarte et secondeCarte et le css permettant de retourner la carte
+Si verrouillage est false :
+
+Cette fonction ajoute à l'enfant double-carte le css permettant de retourner la carte
+1er fois fonction :
+On commence carteRetournee à false
+Si carteRetournee est false on rajoute à premiereCarte la valeur de celle qu'on a cliqué 
+et true à carteRetournee puis la fonction s'arrête
+2ème fois fonction :
+On commence carteRetournee à true
+On rajoute à secondeCarte la valeur de celle qu'on a cliqué et false à carteRetournee
+On ajoute true à verrouillage
+On applique la fonction correspondance
+
+La fonction correspondance indique que :
+si les deux carte ont le même attribut on enlève
+l'évènement par le click de la fonction retournerCarte pour premierCarte et secondCarte
+(on dit aux cartes de rester retournée)
+sinon on laisse les carte*/
